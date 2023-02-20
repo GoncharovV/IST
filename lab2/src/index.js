0fd7db453cea8b31889ff6916f6aa6cf36a58d80
Vadim <Vadim2003Rus@yandex.ru>
2023-02-20 11:49:23 +0300

    Lab3: created base tempalte

---
+++ lab2/src/index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const path = require('path');
+const app = express()
+
+const MOCK_DATA = require('./data.js')
+
+const PORT = 5000
+
+const cards = MOCK_DATA
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+    res.render('index', { cards })
+})
+
+app.get('/cards/:id', (req, res) => {
+    const card = cards.find(card => card.id === Number(req.params.id))
+
+    res.render('photo-page', { card })
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
+})
